119b9abcdadb3bae80dcf4ca789a341c4811a312
CZY <chenzeyuyuze@outlook.com>
2023-04-19 12:33:26 +0800

    .

--- ConnectorPool.cpp
+++ ConnectorPool.cpp
@@ -6,10 +6,11 @@
 bool ConnectorPool::parseJsonFile()
 {
     std::ifstream ifs("dbconf.json");
-    std::ifstream ifs("dbconf.json");
     Json::Reader rd;
     Json::Value root;
     rd.parse(ifs, root);
+
+    is_shutdown = false;
 
     if (root.isObject())
     {
@@ -30,41 +31,122 @@ bool ConnectorPool::parseJsonFile()
 
 ConnectorPool::ConnectorPool()
 {
-    if(!parseJsonFile()){
+    if (!parseJsonFile())
+    {
         printf("error resding JsonFile\n");
         return;
     }
 
-    //初始化连接
-    for(int i = 0; i < m_minSize; i++){
+    // 初始化连接
+    for (int i = 0; i < m_minSize; i++)
+    {
         addConnection();
     }
 
-    std::thread producer(&ConnectorPool::produceConnection,this);
-    std::thread recycler(&ConnectorPool::recycleConnection,this);
+    std::thread producer(&ConnectorPool::produceConnection, this);
+    std::thread recycler(&ConnectorPool::recycleConnection, this);
     producer.detach();
     recycler.detach();
 }
 
-
 void ConnectorPool::addConnection()
 {
-    Connector* conn = new Connector;
-    conn->connect(m_user,m_pd,m_dbName,m_ip,m_port);
+    Connector *conn = new Connector;
+    conn->connect(m_user, m_pd, m_dbName, m_ip, m_port);
     conn->refreshAlivetime();
     m_connQ.push(conn);
 }
 
 void ConnectorPool::produceConnection()
 {
+    while (!is_shutdown)
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_mutexQ);
+            while (m_connQ.size() >= m_minSize && !is_shutdown)
+            {
+                m_condp.wait(lock);
+            }
+        }
+        if (!is_shutdown)
+            addConnection();
+    }
+    std::cout<<"增加线程结束"<<std::endl;
+}
+
+void ConnectorPool::recycleConnection()
+{
+
+    while (!is_shutdown)
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_mutexQ);
+            while (m_connQ.size() < m_maxSize && !is_shutdown)
+            {
+                m_condr.wait(lock);
+            }
+        }
+
+        if (!is_shutdown)
+        {
+            Connector *conn = nullptr;
+            if (!m_connQ.pop(conn))
+            {
+                std::cout << "reduce err" << std::endl;
+            }
+            delete conn;
+        }
+    }
+
+
+    std::cout<<"削减线程结束"<<std::endl;
 }
 
 ConnectorPool::~ConnectorPool()
 {
+    is_shutdown = true;
+    m_condp.notify_all();
+    m_condr.notify_all();
+
     while (m_connQ.empty())
     {
-        Connector * conn = m_connQ.front();
-        m_connQ.pop();
+        Connector *conn = nullptr;
+        if (!m_connQ.pop(conn))
+        {
+            std::cout << "pop err" << std::endl;
+        }
         delete conn;
     }
+}
+
+ConnectorPool *ConnectorPool::GetPool()
+{
+    static ConnectorPool pool;
+
+    return &pool;
+}
+
+std::shared_ptr<Connector> ConnectorPool::getConnector()
+{
+    bool isdequeue;
+    while (m_connQ.empty())
+    {
+        std::unique_lock<std::mutex> lock(m_mutexQ);
+        m_condt.wait_for(lock, std::chrono::milliseconds(m_timeout));
+    }
+    Connector *conn1 = nullptr;
+    if (!m_connQ.pop(conn1))
+    {
+        printf("get pop err \n");
+        return nullptr;
+    }
+    std::shared_ptr<Connector> connptr(conn1, [this](Connector *conn)
+                                       {
+        conn->refreshAlivetime();
+        this->m_connQ.push(conn);
+        this->m_condr.notify_one(); });
+
+    m_condp.notify_one();
+
+    return connptr;
 }
--- ConnectorPool.h
+++ ConnectorPool.h
@@ -3,6 +3,56 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <functional>
+
+template<typename T>
+class SafeQueue
+{
+private:
+    std::mutex m_mutex ;
+
+    std::queue<T> m_queue;
+
+public:
+    SafeQueue(){};
+    ~SafeQueue(){};
+
+
+    int size(){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        return m_queue.size();
+    }
+
+
+    bool empty(){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        return m_queue.empty();
+    }
+
+
+    void push(T &t){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_queue.push(t);
+    }
+
+    bool pop(T &t){
+        std::unique_lock<std::mutex> lock(m_mutex);
+        if(m_queue.empty()){
+            return false;
+        }
+        t = std::move( m_queue.front());
+        m_queue.pop();
+        return true;
+    }
+
+    T & operator[](int i) const
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        return m_queue[i];
+    }
+};
+
+
 
 class ConnectorPool
 {
@@ -16,17 +66,24 @@ private:
     int m_maxSize;
     int m_timeout;
     int m_maxIdleTime;
+    bool is_shutdown;
 
     bool parseJsonFile();
     ConnectorPool();
     void addConnection();
-
-    std::mutex m_mutexQ;
-    std::condition_variable m_cond;
-    std::queue<Connector *> m_connQ;
-
     void produceConnection();
     void recycleConnection();
+
+    std::mutex m_mutexQ;
+    std::condition_variable m_condp;
+    std::condition_variable m_condr;
+    std::condition_variable m_condt;
+    // std::queue<Connector *> m_connQ;
+    SafeQueue<Connector *> m_connQ;
+
+
+
+    
 
 public:
     ~ConnectorPool();
@@ -34,6 +91,10 @@ public:
     ConnectorPool(ConnectorPool &&) = delete;                 // 删除移动构造汉函数
     ConnectorPool &operator=(const ConnectorPool &) = delete; // 删除拷贝构造
     ConnectorPool &operator=(ConnectorPool &&) = delete;      // 移动拷贝函数
+    
+    static ConnectorPool* GetPool();
+    std::shared_ptr<Connector> getConnector();
 
-    ConnectorPool &operator=(const ConnectorPool &obj) = delete; // 删除赋值构造函数
+
+    
 };
